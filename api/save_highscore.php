e33a84ee28cdeb449b7a611bcb21f913df070923
alessiohe-dev <admin@alessiohennecke.de>
2026-07-09 19:05:15 +0200

    Add CORS support to save_highscore.php
    Added CORS header to allow cross-origin requests.

--- api/save_highscore.php
+++ api/save_highscore.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: application/json');
-require_once 'db.php';
+header('Access-Control-Allow-Origin: *');
+require_once __DIR__ . '/../db.php';
 
 $username = $_POST['username'] ?? '';
 $levelId = $_POST['levelId'] ?? '';
