af5dbdf1dc54237987975a893169d5cdefbc6110
alessiohe-dev <admin@alessiohennecke.de>
2026-07-09 19:03:08 +0200

    Enhance CORS support in get_players.php
    Added CORS headers to allow specific methods and headers.

--- api/get_players.php
+++ api/get_players.php
@@ -1,7 +1,9 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-require_once 'db.php';
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+require_once __DIR__ . '/../db.php';
 
 try {
     $pdo = getDBConnection();
